1bd52ea2c505edb308a50d980601e909e50b686c
= <=>
2017-11-30 16:27:06 +0900

    error

--- app/src/main/java/com/example/hyeryeongsong/my_application/MainActivity.java
+++ app/src/main/java/com/example/hyeryeongsong/my_application/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hyeryeongsong.my_application;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,6 +22,17 @@ public class MainActivity extends AppCompatActivity
 
         textView.setTextColor(0xffff0000);
     }
+
+    //button1을 누르면 bt1_Activity 파일로 넘어가게 해주는 코드
+    //Input parameter
+
+    //Intent intent = new Intent(A, B)
+    //startActivity(intent)
+    //이렇게 두 줄로 써도 됨
+
+    //Intent(A, B) --> A:context, B:class
+    startActivity(new Intent(this, bt1_Activity.class));
+
     void bt2_clicked(View v)
     {
         TextView textView;
@@ -35,7 +47,5 @@ public class MainActivity extends AppCompatActivity
 
         textView.setTextColor(0xff0000ff);
     }
-
-
 
 }
---
+++ app/src/main/java/com/example/hyeryeongsong/my_application/bt1_Activity.java
@@ -0,0 +1,32 @@
+package com.example.hyeryeongsong.my_application;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by HyeRyeongSong on 2017. 11. 23..
+ */
+
+public class bt1_Activity extends Activity
+{
+    //extends Activity에 있는 거 가져다가 쓰는 것이므로 @Override 꼭 써줘야 함
+    //안 그러면 충돌남
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bt1_screen);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        ;
+    }
+
+    public void active1_back_clicked(View v)
+    {
+        super.onBackPressed();
+    }
+}
