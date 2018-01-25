c545dd768641825c714e57b618333a7623bfc528
= <=>
2018-01-25 19:29:51 +0900

    Class 5

---
+++ app/src/main/java/com/example/hyeryeongsong/my_application/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.hyeryeongsong.my_application;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    final int RED = 1;
+    final int GREEN = 2;
+    final int BLUE = 3;
+
+    int R_value;
+    int G_value;
+    int B_value;
+
+    View check_box;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        check_box = (View)findViewById(R.id.box);
+
+        R_value = 0;
+        G_value = 0;
+        B_value = 0;
+    }
+
+    void bt1_clicked(View v)
+    {
+        Intent intent = new Intent(this, bt1_Activity.class);
+
+        intent.putExtra("RED", Integer.toString(R_value));
+        intent.putExtra("GREEN", Integer.toString(G_value));
+        intent.putExtra("BLUE", Integer.toString(B_value));
+
+        startActivityForResult(intent, RED);
+    }
+
+    void bt2_clicked(View v)
+    {
+        Intent intent = new Intent(this, bt2_Activity.class);
+
+        intent.putExtra("RED", Integer.toString(R_value));
+        intent.putExtra("GREEN", Integer.toString(G_value));
+        intent.putExtra("BLUE", Integer.toString(B_value));
+
+        startActivityForResult(intent, GREEN);
+    }
+    void bt3_clicked(View v)
+    {
+        Intent intent = new Intent(this, bt3_Activity.class);
+
+        intent.putExtra("RED", Integer.toString(R_value));
+        intent.putExtra("GREEN", Integer.toString(G_value));
+        intent.putExtra("BLUE", Integer.toString(B_value));
+
+        startActivityForResult(intent, BLUE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode == Activity.RESULT_OK)
+        {
+            switch(requestCode)
+            {
+                case RED:
+                    Log.d("color", "R_Num2 : " + data.getStringExtra("RED"));
+                    R_value = Integer.parseInt(data.getStringExtra("RED"));
+                    check_box.setBackgroundColor(Color.rgb(R_value, G_value, B_value));
+                    break;
+
+                case GREEN:
+                    Log.d("color", "G_Num2 : " + data.getStringExtra("GREEN"));
+                    G_value = Integer.parseInt(data.getStringExtra("GREEN"));
+                    check_box.setBackgroundColor(Color.rgb(R_value, G_value, B_value));
+                    break;
+
+                case BLUE:
+                    Log.d("color", "B_Num2 : " + data.getStringExtra("BLUE"));
+                    B_value = Integer.parseInt(data.getStringExtra("BLUE"));
+                    check_box.setBackgroundColor(Color.rgb(R_value, G_value, B_value));
+                    break;
+            }
+        }
+
+        if(resultCode == Activity.RESULT_CANCELED)
+        {
+            Toast.makeText(this, "Activity finished with no return value", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void Go_Clicked(View v)
+    {
+        EditText et = (EditText)findViewById(R.id.t_edit);
+        String str = et.getText().toString();
+
+        //{"words" : "~~~(str)~~~"}
+        //"words"라는 tag를 떼주는 function을 go_Activity에서 해줌
+        Intent intent = new Intent(this, go_Activity.class);
+
+        intent.putExtra("words", str);
+
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/hyeryeongsong/my_application/bt1_Activity.java
@@ -0,0 +1,89 @@
+package com.example.hyeryeongsong.my_application;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by HyeRyeongSong on 2017. 11. 23..
+ */
+
+public class bt1_Activity extends Activity
+{
+    EditText editText;
+    int R_value;
+    int G_value;
+    int B_value;
+
+    View tmp_view;
+
+    boolean valid = false;
+
+    //extends Activity에 있는 거 가져다가 쓰는 것이므로 @Override 꼭 써줘야 함
+    //안 그러면 충돌남
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bt1_screen);
+
+        editText = (EditText)findViewById(R.id.Edit_R_value);
+
+        Intent intent = getIntent();
+
+        R_value = Integer.parseInt(intent.getStringExtra("RED"));
+        G_value = Integer.parseInt(intent.getStringExtra("GREEN"));
+        B_value = Integer.parseInt(intent.getStringExtra("BLUE"));
+
+        tmp_view = (View)findViewById(R.id.t_frame);
+        tmp_view.setBackgroundColor(Color.rgb(R_value, G_value, B_value));
+    }
+
+    public void apply_b_clicked(View v)
+    {
+        R_value = Integer.parseInt(editText.getText().toString());
+
+        if(R_value>=0 && R_value<=255)
+        {
+            tmp_view.setBackgroundColor(Color.rgb(R_value, G_value, B_value));
+            valid = true;
+        }
+        else
+        {
+            Toast.makeText(this, "R value should be in the range (1 ~ 255)", Toast.LENGTH_SHORT).show();
+            valid = false;
+        }
+
+        editText.setText("");
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        ;
+    }
+
+    public void active1_back_clicked(View v)
+    {
+        Intent returnIntent = new Intent();
+
+        if(valid)
+        {
+            returnIntent.putExtra("RED", Integer.toString(R_value));
+
+            Log.d("color", "R_Num1 : " + R_value);
+
+            setResult(Activity.RESULT_OK, returnIntent);
+            super.onBackPressed();
+        }
+        else
+        {
+            Toast.makeText(this, "You should set valid 'R' value!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
